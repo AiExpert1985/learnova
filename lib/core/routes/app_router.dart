a2c100df40fcc28c1a2a2e99b3426f712b63347d
Hamandi <aiexpert2025@gmail.com>
2025-11-27 10:03:08 +0300

    remove unused import

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../../features/qa/screens/qa_screen.dart';
 import '../screens/api_key_missing_screen.dart';
